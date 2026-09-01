95343039c805981e5a1db053d30e5dc1b790bc73
Mizuki Kato <213+mzk@users.noreply.github.com>
2026-09-01 14:31:56 +0900

    DataStoreの定義

--- app/src/main/java/jp/ac/meijou/android/s251205047/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s251205047/MainActivity.java
@@ -1,9 +1,12 @@
 package jp.ac.meijou.android.s251205047;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,6 +16,7 @@ import jp.ac.meijou.android.s251205047.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +31,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        prefDataStore = PrefDataStore.getInstance(this);
 
-        //TextView text = findViewById(R.id.text);
-        binding.text.setText(R.string.my_name);
-        binding.imageView.setImageResource(R.drawable.outline_10k_24);
+        //text変更ボタン
+        binding.button.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            binding.text.setText(text);
+        });
+        //image変更ボタン
+        binding.button2.setOnClickListener(view -> {
+            binding.imageView.setImageResource(R.drawable.outline_10k_24);
+        });
+        //textboxに入力する前/途中/後にアクションできる
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = s.toString();
+                binding.text.setText(text);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+        });
+
+        binding.SaveButton.setOnClickListener(view ->{
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
+        });
     }
 }
