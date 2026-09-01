52ce37306eb59a850212ba3b8b5a5e1b16dac637
Mizuki Kato <213+mzk@users.noreply.github.com>
2026-09-01 12:16:48 +0900

    テキストビューと画像を配置

--- app/src/main/java/jp/ac/meijou/android/s251205047/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s251205047/MainActivity.java
@@ -1,6 +1,7 @@
 package jp.ac.meijou.android.s251205047;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s251205047.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //TextView text = findViewById(R.id.text);
+        binding.text.setText(R.string.my_name);
+        binding.imageView.setImageResource(R.drawable.outline_10k_24);
     }
 }
